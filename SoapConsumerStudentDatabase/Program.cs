1cd81a8204c379024f104a02fe56583683bf6c56
andersbor <anbo@easj.dk>
2017-09-19 21:17:51 +0200

    initial commit

--- SoapConsumerStudentDatabase/Program.cs
+++ SoapConsumerStudentDatabase/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using SoapConsumerStudentDatabase.ServiceReferenceSchool;
 
 namespace SoapConsumerStudentDatabase
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            using (Service1Client client = new Service1Client("BasicHttpBinding_IService1"))
+            {
+                Student[] allStudents = client.GetAllStudents();
+                foreach (var st in allStudents)
+                {
+                    Console.WriteLine(st.Name + " " + st.Semester);
+                }
+
+                Student stu = client.GetStudentById(1);
+                Console.WriteLine(stu.Name + " " + stu.Semester);
+            }
         }
     }
 }
